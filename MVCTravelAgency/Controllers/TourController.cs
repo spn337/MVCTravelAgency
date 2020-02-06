98f9a81890a9b2f9e20d70654a0cc5054a8df150
spn337 <spn337@gmail.com>
2020-02-06 11:36:44 +0200

    Add controller/view

---
+++ MVCTravelAgency/Controllers/TourController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCTravelAgency.Entities;
+using MVCTravelAgency.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MVCTravelAgency.Controllers
+{
+    public class TourController : Controller
+    {
+        private readonly DBContext _context;
+        public TourController(DBContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Tour> Tours = _context.Tours.ToList();
+            return View(Tours);
+        }
+    }
+}
